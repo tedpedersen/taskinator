97fab4fc0b58cc0406aacaa75610bac52a6003dc
Ted Pedersen <tedpedersen@me.com>
2020-06-09 11:32:05 -0700

    js to create tasks

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    event.preventDefault();
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", createTaskHandler);
